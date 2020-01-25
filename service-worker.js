356a9eb66bda4e8e8a535604ff25bca843be29d7
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-01-25 12:38:36 +0700

    tambahan workbox

--- service-worker.js
+++ service-worker.js
@@ -1,88 +1,153 @@
-const CACHE_NAME = "football-apps-v4.1.5";
-const urlsToCache = [
-    "/",
-    "/index.html",
-    "/detail.html",
-    "/nav.html",
-    "/css/materialize.min.css",
-    "/js/api.js",
-    "/js/materialize.min.js",
-    "/js/nav.js",
-    "/js/db.js",
-    "/js/idb.js",
-    "/js/notif.js",
-    "/pages/standing.html",
-    "/pages/team.html",
-    "/pages/favorit.html",
-    "/icon/icon-48.png",
-    "/icon/icon-96.png",
-    "/icon/icon-192.png",
-    "/favicomatic/apple-touch-icon-57x57.png",
-    "/favicomatic/apple-touch-icon-60x60.png",
-    "/favicomatic/apple-touch-icon-72x72.png",
-    "/favicomatic/apple-touch-icon-76x76.png",
-    "/favicomatic/apple-touch-icon-120x120.png",
-    "/favicomatic/apple-touch-icon-114x114.png",
-    "/favicomatic/apple-touch-icon-144x144.png",
-    "/favicomatic/apple-touch-icon-152x152.png",
-    "/favicomatic/favicon-16x16.png",
-    "/favicomatic/favicon-32x32.png",
-    "/favicomatic/favicon-96x96.png",
-    "/favicomatic/favicon-128.png",
-    "/favicomatic/favicon-196x196.png",
-    "/favicomatic/favicon.ico",
-    "/favicomatic/mstile-70x70.png",
-    "/favicomatic/mstile-144x144.png",
-    "/favicomatic/mstile-150x150.png",
-    "/favicomatic/mstile-310x150.png",
-    "/favicomatic/mstile-310x310.png",
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+if (workbox)
+    console.log(`Workbox berhasil dimuat`);
+else
+    console.log(`Workbox gagal dimuat`);
 
-self.addEventListener("install", function (event) {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
-});
-
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener("fetch", function (event) {
-    const base_url = "https://api.football-data.org/v2/";
-    if (event.request.url.indexOf(base_url) > -1) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(function (cache) {
-                return fetch(event.request).then(function (response) {
-                    cache.put(event.request.url, response.clone());
-                    return response;
-                })
-            })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request, {
-                'ignoreSearch': true
-            }).then(function (response) {
-                return response || fetch(event.request);
-            })
-        )
+workbox.precaching.precacheAndRoute([{
+        url: '/',
+        revision: '1'
+    },
+    {
+        url: "/index.html",
+        revision: '1'
+    },
+    {
+        url: '/detail.html',
+        revision: '1'
+    },
+    {
+        url: '/nav.html',
+        revision: '1'
+    },
+    {
+        url: '/css/materialize.min.css',
+        revision: '2'
+    },
+    {
+        url: '/js/materialize.min.js',
+        revision: '1'
+    },
+    {
+        url: '/js/nav.js',
+        revision: '1'
+    },
+    {
+        url: '/js/idb.js',
+        revision: '1'
+    },
+    {
+        url: '/js/db.js',
+        revision: '1'
+    },
+    {
+        url: '/js/notif.js',
+        revision: '1'
+    },
+    {
+        url: '/icon/icon-48.png',
+        revision: '1'
+    },
+    {
+        url: '/icon/icon-96.png',
+        revision: '1'
+    },
+    {
+        url: '/icon/icon-192.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-57x57.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-60x60.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-tauch-icon-72x72.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-76x76.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-120x120.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-114x114.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-144x144.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/apple-touch-icon-152x152.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/favicon-16x16.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/favicon-32x32.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/favicon-96x96.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/favicon-128.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/favicon-196x196.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/favicon.ico',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/mstile-70x70.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/mstile-144x144.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/mstile-150x150.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/mstile-310x150.png',
+        revision: '1'
+    },
+    {
+        url: '/favicomatic/mstile-310x310.png',
+        revision: '1'
     }
-});
+
+]);
+
+workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'pages'
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp('/js/api.js'),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'api'
+    })
+)
 
 self.addEventListener('notificationclick', (event) => {
     event.notification.close(); // agar ketika penguna menekan manakan notifikasi akan hilang
